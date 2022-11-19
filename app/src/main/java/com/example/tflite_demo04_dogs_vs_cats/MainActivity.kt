bd26d83d21168069de9dce8b55abeb82b2c4bc49
xiaoxiong1006 <758063761@qq.com>
2022-11-19 17:00:43 +0800

    step01 add dependencies

---
+++ app/src/main/java/com/example/tflite_demo04_dogs_vs_cats/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tflite_demo04_dogs_vs_cats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
