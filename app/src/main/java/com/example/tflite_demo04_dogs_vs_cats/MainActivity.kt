2b5e08c0a9b47d59fb3f03149e2e5a8924e3b46e
xiaoxiong1006 <758063761@qq.com>
2022-11-19 17:46:35 +0800

    step5 add photo capture function 增加拍摄功能

--- app/src/main/java/com/example/tflite_demo04_dogs_vs_cats/MainActivity.kt
+++ app/src/main/java/com/example/tflite_demo04_dogs_vs_cats/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.tflite_demo04_dogs_vs_cats
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.TextureView
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import java.io.File
+import java.util.concurrent.Executors
 
 //这是设置请求权限的code码
 private const val REQUEST_CODE_PERMISSIONS = 1
@@ -21,6 +24,7 @@ private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA,
     Manifest.permission.READ_EXTERNAL_STORAGE)
 
 class MainActivity : AppCompatActivity() {
+    private var imageCapture:ImageCapture? = null
     private lateinit var view_finder:TextureView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +41,41 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this,
                 REQUIRED_PERMISSIONS,
                 REQUEST_CODE_PERMISSIONS)
+        }
+
+        val btn_take = findViewById<ImageView>(R.id.btn_take)
+        val executor = Executors.newSingleThreadExecutor()
+        btn_take.setOnClickListener {
+            //创建文件
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg");
+
+            imageCapture?.takePicture(file, executor,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(
+                        imageCaptureError: ImageCapture.ImageCaptureError,
+                        message: String,
+                        exc: Throwable?
+                    ) {
+                        //保存失败
+                        val msg = "保存失败: $message"
+                        view_finder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        //保存成功
+                        val msg = "保存成功: ${file.absolutePath}"
+                        view_finder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                            //这个广播的目的就是更新图库，发了这个广播进入相册就可以找到你保存的图片了！
+                            val intent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+                            val uri = Uri.fromFile(file)
+                            intent.data = uri
+                            sendBroadcast(intent)
+                        }
+                    }
+                })
         }
     }
 
@@ -89,7 +128,19 @@ class MainActivity : AppCompatActivity() {
             view_finder.setSurfaceTexture(it.surfaceTexture)
         }
 
+        // 为图像捕获用例创建配置对象
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                //设置图像捕获模式
+                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                //设置前后摄像头
+                setLensFacing(CameraX.LensFacing.BACK)
+            }.build()
+
+        // 构建图像捕获用例
+        imageCapture = ImageCapture(imageCaptureConfig)
+
         // 将用例绑定到生命周期
-        CameraX.bindToLifecycle(this as LifecycleOwner, preview)
+        CameraX.bindToLifecycle(this as LifecycleOwner, preview,imageCapture)
     }
 }
